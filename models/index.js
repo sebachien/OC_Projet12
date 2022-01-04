76b3dce411debda76504e6042ba653c225f5d324
@sebachien <sebastien.louis.69@gmail.com>
2022-01-04 17:27:11 +0100

    mongoose to postgres v27

--- models/index.js
+++ models/index.js
@@ -23,8 +23,19 @@ const db = {};
 sequelize.sync({ force: true });
 console.log("All models were synchronized successfully.");
 
+sequelize
+  .query('SELECT * FROM salesforce.contact', { model: contact })
+  .then(projects => {
+    // Each record will now be mapped to the project's model.
+    console.log(projects)
+  })
+
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 //db.contactSchema = require("../models/Contact")(sequelize, Sequelize);
+
+
+
+
 module.exports = db;
