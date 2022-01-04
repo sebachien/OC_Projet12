deb3687ef7e62137b46d6f210579a72b3bb33a2c
@sebachien <sebastien.louis.69@gmail.com>
2022-01-04 12:14:05 +0100

    add pg connection v1

--- app.js
+++ app.js
@@ -13,7 +13,25 @@ mongoose.connect('mmongodb+srv://admin:admin@openclassroom1.qq2d4.mongodb.net/my
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
+  const { Client } = require('pg');
 
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  
+  client.connect();
+  
+  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  });
 
 const app = express()
 app.use((req, res, next) => {
