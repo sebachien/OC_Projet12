7afdf8587a4d12df19c4a64253d18263f280af4e
@sebachien <sebastien.louis.69@gmail.com>
2022-01-04 14:48:26 +0100

    mongoose to postgres v9

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
-        ssl: true
+        ssl: false
     }
 });
 
