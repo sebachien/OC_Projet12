c1126a49d625ad129cc0e18d073e722c10fc48a7
@sebachien <sebastien.louis.69@gmail.com>
2022-01-04 14:59:28 +0100

    mongoose to postgres v10

--- models/index.js
+++ models/index.js
@@ -1,11 +1,22 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
     dialectOptions: {
-        ssl: false
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-});
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 const db = {};
 
