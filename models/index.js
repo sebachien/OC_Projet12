daa3a7426be63cbe384a8c6f1976bc47517a4f3d
@sebachien <sebastien.louis.69@gmail.com>
2022-01-04 16:53:44 +0100

    mongoose to postgres v26

--- models/index.js
+++ models/index.js
@@ -20,7 +20,7 @@ sequelize
 
 const db = {};
 
-//await sequelize.sync({ force: true });
+sequelize.sync({ force: true });
 console.log("All models were synchronized successfully.");
 
 db.Sequelize = Sequelize;
