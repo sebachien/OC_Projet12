7570b174751c977afe2302515d2a1146f2d21331
@sebachien <sebastien.louis.69@gmail.com>
2022-01-04 18:23:38 +0100

    mongoose to postgres v39

--- controllers/contact.js
+++ controllers/contact.js
@@ -42,6 +42,7 @@ exports.getOneContact = (req, res, next) => {
 
 exports.getAllContact =  (req, res, next) => {
   console.log("Op :"+Op);
+  console.log("req :"+req);
   console.log("Contact :"+Contact);
   Contact.findAll()
     .then(contact => res.status(200).json(contact))
