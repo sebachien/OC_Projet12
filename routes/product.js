980797be9cfbd912fd6c5ac362f4eecdc54780e3
@sebachien <sebastien.louis.69@gmail.com>
2022-01-05 22:32:51 +0100

    add product and contract v13

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const productCtrl = require('../controllers/contract');
+const productCtrl = require('../controllers/product');
 const auth = require('../middleware/auth');
 
 
