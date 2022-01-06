b4e779e86176a189c7b599a3661df2430c7084b8
@sebachien <sebastien.louis.69@gmail.com>
2022-01-06 07:30:18 +0100

    add product and contract v92

--- controllers/contract.js
+++ controllers/contract.js
@@ -32,7 +32,12 @@ exports.getAllContract =  (req, res, next) => {
 };
 
 exports.modifyContract = (req, res, next) => {
-  const contract = Contract.findByPk(req.params.sfid);
+  const contract = Contract.findByPk(req.params.sfid).then((res) => {
+    console.log(contract);
+  Contract.update(req.body, {where :{sfid: req.params.sfid}})
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ errore: 'Objet non modifié !' }));
+})
   console.log(contract);
   Contract.update(req.body, {where :{sfid: req.params.sfid}})
     .then(() => res.status(200).json({ message: 'Objet modifié !'}))
